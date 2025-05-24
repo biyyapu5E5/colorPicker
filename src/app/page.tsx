5b4537dfa549d7fb6c83746eb86a6a7107f98e66
padmini <pbiyyapu02@gmail.com>
2025-05-24 17:07:00 +0530

    navigated to route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Link from 'next/link';
+import {useRouter} from 'next/router';
 
 export default function HomePage() {
+  const router = useRouter();
+  router.push('/color');
   return (
     <div>
       <p>Home Page</p>
-      <Link href='color'>Color</Link>
+      {/* <Link href='color'>Color</Link> */}
     </div>
   )
 }
